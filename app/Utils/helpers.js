ab054c474bfa0fe6a498d369402d8e766597a165
Joseph Abell <jsphabll@gmail.com>
2016-02-23 08:36:22 +0000

    adding origin to api request

--- app/Utils/helpers.js
+++ app/Utils/helpers.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 function getPage() {
 	return axios.get(`https://en.wikipedia.org/w/api.php?format=jsonfm&action=query&generator=random&grnnamespace=0&prop=revisions|images&rvprop=content&grnlimit=1/https://en.wikipedia.org/w/api.php?format=jsonfm&action=query&generator=random&grnnamespace=0&prop=revisions|images&rvprop=content&grnlimit=1&uselang=user/`, {
-		headers: {'API-User-Agent': 'Wikilynx/0.1' }
+		headers: {'API-User-Agent': 'Wikilynx/0.1', 'origin': 'http://gerbilsinspace.github.io' }
 	});
 }
 
