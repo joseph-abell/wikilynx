368e67692e1a3bad4783c3765b5257cf6cd98047
Joseph Abell <jsphabll@gmail.com>
2016-02-23 22:05:51 +0000

    More changes

--- app/Utils/helpers.js
+++ app/Utils/helpers.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
-var toWiki = axios.create({
-	headers: {
-		"Origin": "http://gerbilsinspace.github.io",
-		"Content-Type": "application/json; charset=UTF-8"
-	}
-});
-
 function getPage() {
-	return toWiki.get(`https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&prop=revisions|images&rvprop=content&grnlimit=1/https://en.wikipedia.org/w/api.php?format=jsonfm&action=query&generator=random&grnnamespace=0&prop=revisions|images&rvprop=content&grnlimit=1&uselang=user/`);
+	return axios({
+		method: 'get',
+		url: 'https://en.wikipedia.org/w/api.php',
+		format: 'json',
+		action: 'query',
+		generator: 'random',
+		grnamespace: 0,
+		prop: 'revisions',
+		rvprop: 'content',
+		grnlimit: 1,
+		useLang: 'user'
+	});
 }
 
 export default function getPages() {
-	return toWiki.all([getPage(), getPage()])
+	return axios.all([getPage(), getPage()])
 		.then( (arr) => ({ firstPage: arr[0].data, secondPage: arr[1].data }));
 }
