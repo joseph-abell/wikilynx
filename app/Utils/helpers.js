ea302d4c179534155b9932c89a030f16fbe0cee2
Joseph Abell <jsphabll@gmail.com>
2016-02-23 19:08:09 +0000

    adding cords header rules, hopefully this will work.

--- app/Utils/helpers.js
+++ app/Utils/helpers.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 
+var toWiki = axios.create({
+	headers: {
+		"Origin": "http://gerbilsinspace.github.io",
+		"Content-Type": "application/json; charset=UTF-8"
+	}
+});
+
 function getPage() {
-	return axios.get(`https://en.wikipedia.org/w/api.php?format=jsonfm&action=query&generator=random&grnnamespace=0&prop=revisions|images&rvprop=content&grnlimit=1/https://en.wikipedia.org/w/api.php?format=jsonfm&action=query&generator=random&grnnamespace=0&prop=revisions|images&rvprop=content&grnlimit=1&uselang=user/`, {
-		headers: {'API-User-Agent': 'Wikilynx/0.1', 'origin': 'http://gerbilsinspace.github.io' }
-	});
+	return toWiki.get(`https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&prop=revisions|images&rvprop=content&grnlimit=1/https://en.wikipedia.org/w/api.php?format=jsonfm&action=query&generator=random&grnnamespace=0&prop=revisions|images&rvprop=content&grnlimit=1&uselang=user/`);
 }
 
 export default function getPages() {
-	return axios.all([getPage(), getPage()])
+	return toWiki.all([getPage(), getPage()])
 		.then( (arr) => ({ firstPage: arr[0].data, secondPage: arr[1].data }));
 }
