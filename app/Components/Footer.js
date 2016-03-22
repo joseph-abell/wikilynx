dcf23bb067b7800e90172be0a40be95fbbc9ab51
Joseph Abell <jsphabll@gmail.com>
2016-03-22 14:12:57 +0000

    fix footer positioning

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = React.createClass({
 	render: () => {
 		return (
-			<div style={{position: 'absolute', bottom: 0}}>
+			<div style={{paddingTop: 40}}>
 				<p>Wikipedia® is a registered trademark of the Wikimedia Foundation, Inc., a non-profit organization</p>
 				<p><a href="http://wikimediafoundation.org/wiki/Support_Wikipedia/en">Please Donate</a> to support Wikipedia</p>
 			</div>
