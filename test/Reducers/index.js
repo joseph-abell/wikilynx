1e182b64febad0199509c2beb3c3ce054a494ca3
Joseph Abell <jsphabll@gmail.com>
2016-03-23 08:23:31 +0000

    Adding more tests to reducers

--- test/Reducers/index.js
+++ test/Reducers/index.js
@@ -155,4 +155,364 @@ describe('reducers', () => {
 			expect(actual).toEqual(expected);
 		});
 	});
+
+	describe('currentPage', () => {
+		it('should set the currentPage title to Virtual Reality, no links', () => {
+			const actual = gameApp(stateBefore(), {
+				type: GET_CURRENT_PAGE,
+				currentPage: {
+					title: 'Virtual Reality',
+					links: []
+				}
+			});
+			
+			const expected = {
+				breadcrumbs: [],
+				completeGame: {
+					isCompleted: false
+				},
+				currentPage: {
+					links: [],
+					title: 'Virtual Reality'
+				},
+				firstPage: '',
+				gameBoardLoading: false,
+				lastPage: '',
+				newGame: true,
+				newGameLoading: false,
+				viewer: {
+					title: '',
+					content: ''
+				},
+				viewerLoading: false
+			};
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should set the currentPage title to The Streets, with links', () => {
+			const actual = gameApp(stateBefore(), {
+				type: GET_CURRENT_PAGE,
+				currentPage: {
+					title: 'The Streets',
+					links: ['Adele', 'Fun.', 'The Streets']
+				}
+			});
+			
+			const expected = {
+				breadcrumbs: [],
+				completeGame: {
+					isCompleted: false
+				},
+				currentPage: {
+					links: ['Adele', 'Fun.', 'The Streets'],
+					title: 'The Streets'
+				},
+				firstPage: '',
+				gameBoardLoading: false,
+				lastPage: '',
+				newGame: true,
+				newGameLoading: false,
+				viewer: {
+					title: '',
+					content: ''
+				},
+				viewerLoading: false
+			};
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should allow resetting currentPage', () => {
+			const dirtyState = gameApp(stateBefore(), {
+				type: GET_CURRENT_PAGE,
+				currentPage: {
+					title: 'Warioware',
+					links: ['Mario 64', 'Games', 'Project Cars']
+				}
+			});
+
+			const actual = gameApp(dirtyState, {
+				type: GET_CURRENT_PAGE,
+				currentPage: {
+					title: '',
+					links: []
+				}
+			});
+
+			const expected = {
+				breadcrumbs: [],
+				completeGame: {
+					isCompleted: false
+				},
+				currentPage: {
+					links: [],
+					title: ''
+				},
+				firstPage: '',
+				gameBoardLoading: false,
+				lastPage: '',
+				newGame: true,
+				newGameLoading: false,
+				viewer: {
+					title: '',
+					content: ''
+				},
+				viewerLoading: false
+			};
+
+			expect(actual).toEqual(expected);
+		});
+	});
+
+	describe('viewer', () => {
+		it('should set the viewer title to HTC Vive, no content', () => {
+			const actual = gameApp(stateBefore(), {
+				type: GET_VIEWER,
+				viewer: {
+					title: 'HTC Vive',
+					content: ''
+				}
+			});
+			
+			const expected = {
+				breadcrumbs: [],
+				completeGame: {
+					isCompleted: false
+				},
+				currentPage: {
+					links: [],
+					title: ''
+				},
+				firstPage: '',
+				gameBoardLoading: false,
+				lastPage: '',
+				newGame: true,
+				newGameLoading: false,
+				viewer: {
+					title: 'HTC Vive',
+					content: ''
+				},
+				viewerLoading: false
+			};
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should set the viewer title to Playstation Move, with content', () => {
+			const actual = gameApp(stateBefore(), {
+				type: GET_VIEWER,
+				viewer: {
+					title: 'Playstation Move',
+					content: 'Do, do, do... Come on and do the Conga'
+				}
+			});
+			
+			const expected = {
+				breadcrumbs: [],
+				completeGame: {
+					isCompleted: false
+				},
+				currentPage: {
+					links: [],
+					title: ''
+				},
+				firstPage: '',
+				gameBoardLoading: false,
+				lastPage: '',
+				newGame: true,
+				newGameLoading: false,
+				viewer: {
+					title: 'Playstation Move',
+					content: 'Do, do, do... Come on and do the Conga'
+				},
+				viewerLoading: false
+			};
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should allow resetting viewer', () => {
+			const dirtyState = gameApp(stateBefore(), {
+				type: GET_VIEWER,
+				viewer: {
+					title: 'Star Wars',
+					content: 'Fake Hans Solo lands, and R2D2 gets off.'
+				}
+			});
+
+			const actual = gameApp(dirtyState, {
+				type: GET_VIEWER,
+				viewer: {
+					title: '',
+					content: ''
+				}
+			});
+
+			const expected = {
+				breadcrumbs: [],
+				completeGame: {
+					isCompleted: false
+				},
+				currentPage: {
+					links: [],
+					title: ''
+				},
+				firstPage: '',
+				gameBoardLoading: false,
+				lastPage: '',
+				newGame: true,
+				newGameLoading: false,
+				viewer: {
+					title: '',
+					content: ''
+				},
+				viewerLoading: false
+			};
+
+			expect(actual).toEqual(expected);
+		});
+	});
+
+	describe('addBreadcrumb', () => {
+		it('should add breadcrumb', () => {
+			const actual = gameApp(stateBefore(), {
+				type: ADD_BREADCRUMB,
+				title: 'Archery'
+			});
+			
+			const expected = {
+				breadcrumbs: [
+					{
+						title: 'Archery'
+					}
+				],
+				completeGame: {
+					isCompleted: false
+				},
+				currentPage: {
+					links: [],
+					title: ''
+				},
+				firstPage: '',
+				gameBoardLoading: false,
+				lastPage: '',
+				newGame: true,
+				newGameLoading: false,
+				viewer: {
+					title: '',
+					content: ''
+				},
+				viewerLoading: false
+			};
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should add multiple breadcrumbs', () => {
+			const firstBreadcrumb = gameApp(stateBefore(), {
+				type: ADD_BREADCRUMB,
+				title: 'First Person Shooter'
+			});
+
+			const secondBreadcrumb = gameApp(firstBreadcrumb, {
+				type: ADD_BREADCRUMB,
+				title: 'The Matrix'
+			});
+
+			const thirdBreadcrumb = gameApp(secondBreadcrumb, {
+				type: ADD_BREADCRUMB,
+				title: 'Netflix'
+			});
+
+			const expected = {
+				breadcrumbs: ['First Person Shooter', 'The Matrix', 'Netflix'],
+				completeGame: {
+					isCompleted: false
+				},
+				currentPage: {
+					links: [],
+					title: ''
+				},
+				firstPage: '',
+				gameBoardLoading: false,
+				lastPage: '',
+				newGame: true,
+				newGameLoading: false,
+				viewer: {
+					title: '',
+					content: ''
+				},
+				viewerLoading: false
+			}
+		});
+	});
+
+	describe('resetBreadcrumb', () => {
+		it('should reset breadcrumbs that had breadcrumbs', () => {
+			const firstBreadcrumb = gameApp(stateBefore(), {
+				type: ADD_BREADCRUMB,
+				title: 'Portal'
+			});
+
+			const secondBreadcrumb = gameApp(firstBreadcrumb, {
+				type: ADD_BREADCRUMB,
+				title: 'Soccer'
+			});
+
+			const actual = gameApp(secondBreadcrumb, {
+				type: RESET_BREADCRUMB
+			});			
+			
+			const expected = {
+				breadcrumbs: [],
+				completeGame: {
+					isCompleted: false
+				},
+				currentPage: {
+					links: [],
+					title: ''
+				},
+				firstPage: '',
+				gameBoardLoading: false,
+				lastPage: '',
+				newGame: true,
+				newGameLoading: false,
+				viewer: {
+					title: '',
+					content: ''
+				},
+				viewerLoading: false
+			};
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should reset an empty breadcrumbs', () => {
+			const actual = gameApp(stateBefore(), {
+				type: RESET_BREADCRUMB
+			});
+
+			const expected = {
+				breadcrumbs: [],
+				completeGame: {
+					isCompleted: false
+				},
+				currentPage: {
+					links: [],
+					title: ''
+				},
+				firstPage: '',
+				gameBoardLoading: false,
+				lastPage: '',
+				newGame: true,
+				newGameLoading: false,
+				viewer: {
+					title: '',
+					content: ''
+				},
+				viewerLoading: false
+			}
+		});
+	});
 });
